e2de405f8204cdacbf278df7f11fc0702fff0871
Steven Barrett <damentz@liquorix.net>
2017-05-30 23:18:54 -0500

    ZEN: Implement Zen-Tune v4.20
    4.9:
    In a surprising turn of events, while benchmarking and testing
    hierarchical scheduling with BFQ + writeback throttling, it turns out
    that raising the number of requests in queue _actually_ improves
    responsiveness and completely eliminates the random stalls that would
    normally occur without hierarchical scheduling.
    
    To make this test more intense, I used the following test:
    
    Rotational disk1: rsync -a /source/of/data /target/to/disk1
    Rotational disk2: rsync -a /source/of/data /target/to/disk2
    
    And periodically attempted to write super fast with:
    dd if=/dev/zero of=/target/to/disk1/block bs=4096
    
    This wrote 10gb incredibly fast to writeback and I encountered zero
    stalls through this entire test of 10-15 minutes.
    
    My suspicion is that with cgroups, BFQ is more able to properly sort
    among multiple drives, reducing the chance of a starved process.  This
    plus writeback throttling completely eliminate any outstanding bugs with
    high writeback ratios, letting the user enjoy low latency writes
    (application thinks they're already done), and super high throughput due
    to batched writes in writeback.
    
    Please note however, without the following configuration, I cannot
    guarantee you will not get stalls:
    
    CONFIG_BLK_CGROUP=y
    CONFIG_CGROUP_WRITEBACK=y
    CONFIG_IOSCHED_CFQ=y
    CONFIG_CFQ_GROUP_IOSCHED=y
    CONFIG_IOSCHED_BFQ=y
    CONFIG_BFQ_GROUP_IOSCHED=y
    CONFIG_DEFAULT_BFQ=y
    CONFIG_SCSI_MQ_DEFAULT=n
    
    Special thanks to h2, author of smxi and inxi, for providing evidence
    that a configuration specific to Debian did not cause stalls found the
    Liquorix kernels under heavy IO load.  This specific configuration
    turned out to be hierarchical scheduling on CFQ (thus, BFQ as well).
    
    4.10:
    During some personal testing with the Dolphin emulator, MuQSS has
    serious problems scaling its frequencies causing poor performance where
    boosting the CPU frequencies would have fixed them.  Reducing the
    up_threshold to 45 with MuQSS appears to fix the issue, letting the
    introduction to "Star Wars: Rogue Leader" run at 100% speed versus about
    80% on my test system.
    
    Also, lets refactor the definitions and include some indentation to help
    the reader discern what the scope of all the macros are.
    
    4.11:
    Increase MICRO_FREQUENCY_UP_THRESHOLD from 95 to 85
    Increase MIN_FREQUENCY_UP_THRESHOLD from 11 to 6
    
    These changes should help make using CFS feel a bit more responsive when
    working with mostly idle workloads, browsing the web, scrolling through
    text, etc.
    
    Increasing the minimum frequency up threshold to 6% may be too
    aggressive though.  Will revert this setting if it causes significant
    battery drain.
    
    4.12:
    Make bfq the default MQ scheduler
    
    Reduce default sampling down factor from 10 to 1
    
    With the world eventually moving to a more laptop heavy configuration,
    it's getting more important that we can reduce our frequencies quickly
    after performing work.  This is normal with a ton of background
    processes that need to perform burst work then sleep.
    
    Since this doesn't really impact performance too much, lets not keep it
    part of ZEN_INTERACTIVE.
    
    Some time ago, the minimum frequency up threshold was set to 1 by
    default, but the zen configuration was never updated to take advantage
    of it.
    
    Remove custom MIN_FREQUENCY_UP_THRESHOLD for MuQSS / ZEN_INTERACTIVE
    configurations and make 1 the default for all choices.
    
    Change-Id: I2a31fbc97fe12ffce30457ec2e83ed25764e2daf
    Signed-off-by: Harsh Shandilya <msfjarvis@gmail.com>
    Signed-off-by: Panchajanya1999 <rsk52959@gmail.com>

--- kernel/sched/fair.c
+++ kernel/sched/fair.c
@@ -49,8 +49,13 @@
  * (to see the precise effective timeslice length of your workload,
  *  run vmstat and monitor the context-switches (cs) field)
  */
-unsigned int sysctl_sched_latency = 6000000ULL;
-unsigned int normalized_sysctl_sched_latency = 6000000ULL;
+#ifdef CONFIG_ZEN_INTERACTIVE
+unsigned int sysctl_sched_latency			= 4000000ULL;
+unsigned int normalized_sysctl_sched_latency		= 4000000ULL;
+#else
+unsigned int sysctl_sched_latency			= 6000000ULL;
+unsigned int normalized_sysctl_sched_latency		= 6000000ULL;
+#endif
 
 unsigned int sysctl_sched_sync_hint_enable = 1;
 unsigned int sysctl_sched_cstate_aware = 1;
@@ -71,8 +76,13 @@ enum sched_tunable_scaling sysctl_sched_tunable_scaling
  * Minimal preemption granularity for CPU-bound tasks:
  * (default: 0.75 msec * (1 + ilog(ncpus)), units: nanoseconds)
  */
-unsigned int sysctl_sched_min_granularity = 750000ULL;
-unsigned int normalized_sysctl_sched_min_granularity = 750000ULL;
+#ifdef CONFIG_ZEN_INTERACTIVE
+unsigned int sysctl_sched_min_granularity		= 400000ULL;
+unsigned int normalized_sysctl_sched_min_granularity	= 400000ULL;
+#else
+unsigned int sysctl_sched_min_granularity		= 750000ULL;
+unsigned int normalized_sysctl_sched_min_granularity	= 750000ULL;
+#endif
 
 /*
  * is kept at sysctl_sched_latency / sysctl_sched_min_granularity
@@ -93,10 +103,17 @@ unsigned int sysctl_sched_child_runs_first __read_mostly;
  * and reduces their over-scheduling. Synchronous workloads will still
  * have immediate wakeup/sleep latencies.
  */
-unsigned int sysctl_sched_wakeup_granularity = 1000000UL;
-unsigned int normalized_sysctl_sched_wakeup_granularity = 1000000UL;
+#ifdef CONFIG_ZEN_INTERACTIVE
+unsigned int sysctl_sched_wakeup_granularity		= 500000UL;
+unsigned int normalized_sysctl_sched_wakeup_granularity	= 500000UL;
 
-const_debug unsigned int sysctl_sched_migration_cost = 500000UL;
+const_debug unsigned int sysctl_sched_migration_cost	= 250000UL;
+#else
+unsigned int sysctl_sched_wakeup_granularity		= 1000000UL;
+unsigned int normalized_sysctl_sched_wakeup_granularity	= 1000000UL;
+
+const_debug unsigned int sysctl_sched_migration_cost	= 500000UL;
+#endif
 
 /*
  * The exponential sliding  window over which load is averaged for shares
@@ -116,7 +133,11 @@ unsigned int __read_mostly sysctl_sched_shares_window = 10000000UL;
  *
  * default: 5 msec, units: microseconds
   */
-unsigned int sysctl_sched_cfs_bandwidth_slice = 5000UL;
+#ifdef CONFIG_ZEN_INTERACTIVE
+unsigned int sysctl_sched_cfs_bandwidth_slice		= 3000UL;
+#else
+unsigned int sysctl_sched_cfs_bandwidth_slice		= 5000UL;
+#endif
 #endif
 
 /*
