7f4675705c69859456d99d6ce7561f2d980ae20f
Rizky Benggolo <travarilo@gmail.com>
2019-03-24 14:33:29 +0700

    input: cdfinger: Boost CPU and DDR bus on interrupt
    * Implement https://github.com/kdrag0n/proton_bluecross/commit/c36fe109519db0589ec29a5a4f42e0c8920f90a2
    
    Change-Id: I044c2dbde7dc91695b309861be92076d561494d0

--- drivers/input/fingerprint/cdfinger_X00T/cdfingerfp.c
+++ drivers/input/fingerprint/cdfinger_X00T/cdfingerfp.c
@@ -40,6 +40,9 @@
 #include <linux/regulator/consumer.h>
 #include <linux/fb.h>
 #include <linux/notifier.h>
+#include <linux/cpu_input_boost.h>
+#include <linux/devfreq_boost.h>
+#include <linux/display_state.h>
 #include "../common_X00T/fingerprint_common.h"
 
 typedef struct key_report {
@@ -359,6 +362,12 @@ static irqreturn_t cdfinger_eint_handler(int irq, void *dev_id)
 		cdfinger_wake_lock(pdata,1);
 		cdfinger_async_report();
 	}
+
+	if (!is_display_on()) {
+		cpu_input_boost_kick_wake();
+		devfreq_boost_kick_wake(DEVFREQ_MSM_CPUBW);
+	}
+
 	return IRQ_HANDLED;
 }
 
