eeda551967fb67f5260d5c1d3e19a84625fcf775
celtare21 <celtare21@gmail.com>
2019-03-31 10:47:25 +0000

    cpufreq: schedutil: Lower down rate limit
    Based on https://github.com/kdrag0n/proton_bluecross/commit/11767a2bd2b687ad82d4bd6d4d024232bab48d8d
    
    Signed-off-by: celtare21 <celtare21@gmail.com>
    Signed-off-by: Dusan Uveric <dusan.uveric9@gmail.com>

--- kernel/sched/cpufreq_schedutil.c
+++ kernel/sched/cpufreq_schedutil.c
@@ -718,7 +718,7 @@ static int sugov_init(struct cpufreq_policy *policy)
 
         /* Hard-code some sane rate-limit values */
         tunables->up_rate_limit_us = 10000;
-        tunables->down_rate_limit_us = 20000;
+        tunables->down_rate_limit_us = 10000;
 
 	policy->governor_data = sg_policy;
 	sg_policy->tunables = tunables;
