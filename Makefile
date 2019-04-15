942ec5f7777e7b86efda92a852c8144dd9ce0125
DarknessKiller <kaelvinchen2@gmail.com>
2019-04-15 18:27:21 +0800

    BuildTools: Vanish Clang Build Tools
    * We Will Use GCC Now..

--- Makefile
+++ Makefile
@@ -392,6 +392,7 @@ KBUILD_CPPFLAGS := -D__KERNEL__
 KBUILD_CFLAGS   := -Wall -Wundef -Wstrict-prototypes -Wno-trigraphs \
 		   -fno-strict-aliasing -fno-common \
 		   -Werror-implicit-function-declaration \
+		   -Wno-error=misleading-indentation \
 		   -Wno-format-security \
 		   -std=gnu89 $(call cc-option,-fno-PIE)
 
--- build.sh
+++ build.sh
@@ -1,20 +1,17 @@
-	#!/bin/bash
-	clear
-	echo OverdoseKernel Compile Tool
-	echo By MrDarkness
-	echo Please Sit Back And Chill...
+    #!/bin/bash
+    clear
+    echo OverdoseKernel Compile Tool
+    echo By MrDarkness
+    echo Please Sit Back And Chill...
 	export MAIN=`readlink -f ../`
 	export KERNELDIR=`readlink -f .`
 	rm -rf $KERNELDIR/built
 	mkdir -p $KERNELDIR/built/
-	make ARCH=arm64 X00T_defconfig
-	make -j$(nproc --all) ARCH=arm64 \
-                      CC="$MAIN/aosp-clang/bin/clang" \
-                      CLANG_TRIPLE=aarch64-linux-gnu- \
-                      CROSS_COMPILE="$MAIN/aarch64-linux-gnu/bin/aarch64-linux-gnu-"
-	rm -rf $KERNELDIR/built
-	mkdir -p $KERNELDIR/built/
+    export CROSS_COMPILE=$MAIN/aarch64-linux-gnu/bin/aarch64-linux-gnu-   
+    export ARCH=arm64
+    make X00T_defconfig
+    make -j$(nproc --all) | tee $KERNELDIR/built/kernel_output.txt
 	mv $KERNELDIR/arch/arm64/boot/Image.gz-dtb $KERNELDIR/built/Image.gz-dtb
+    echo ""
+    echo "Compile Done"
 
-	echo ""
-	echo "Compile Done"
