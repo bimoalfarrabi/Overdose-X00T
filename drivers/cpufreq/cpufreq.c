1af9f7ceae5ca90f54340dd1f745a5473048e14a
DarknessKiller <kaelvinchen2@gmail.com>
2019-04-20 09:30:13 +0800

    Revert "treewide: Update usages of boost task check helper"
    This reverts commit d7e41bd4f6fcdaeb733968ecdf04fc927afc48ba.

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -746,9 +746,7 @@ static ssize_t show_cpuinfo_cur_freq(struct cpufreq_policy *policy,
  */
 static ssize_t show_scaling_governor(struct cpufreq_policy *policy, char *buf)
 {
-	if (task_is_booster(current))
-		return sprintf(buf, "schedutil\n");
-	else if (policy->policy == CPUFREQ_POLICY_POWERSAVE)
+	if (policy->policy == CPUFREQ_POLICY_POWERSAVE)
 		return sprintf(buf, "powersave\n");
 	else if (policy->policy == CPUFREQ_POLICY_PERFORMANCE)
 		return sprintf(buf, "performance\n");
