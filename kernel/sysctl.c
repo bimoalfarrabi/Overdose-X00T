5a5a343e641d3654f4d8401debf7d1ed8b530314
Tyler Nijmeh <tylernij@gmail.com>
2019-05-11 17:01:45 -0700

    sched/fair: Introduce sysctl interface for SchedTune cluster bias
    This commit was originally developed by joshuous @ GitHub as a sched
    feature. This implementation allows the bias to be toggled during
    runtime.
    
    Signed-off-by: Tyler Nijmeh <tylernij@gmail.com>

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -301,7 +301,7 @@ static struct ctl_table kern_table[] = {
 		.maxlen		= sizeof(unsigned int),
 		.mode		= 0644,
 		.proc_handler	= proc_dointvec,
-	},
+	},	
 #if defined(CONFIG_PREEMPT_TRACER) || defined(CONFIG_IRQSOFF_TRACER)
 	{
 		.procname       = "preemptoff_tracing_threshold_ns",
@@ -319,6 +319,13 @@ static struct ctl_table kern_table[] = {
 	},
 #endif
 #ifdef CONFIG_SCHED_DEBUG
+	{
+		.procname	= "sched_cpu_schedtune_bias",
+		.data		= &sysctl_sched_cpu_schedtune_bias,
+		.maxlen		= sizeof(unsigned int),
+		.mode		= 0644,
+		.proc_handler	= proc_dointvec,
+	},
 	{
 		.procname	= "sched_min_granularity_ns",
 		.data		= &sysctl_sched_min_granularity,
