347ab0dc2be30e1d811c68a351dbaf1536f865df
DarknessKiller <kaelvinchen2@gmail.com>
2019-06-07 11:51:12 +0800

    BuildTools: GCC 4.9 Toolchain

--- build.sh
+++ build.sh
@@ -1,21 +1,17 @@
-    #!/bin/bash
-    clear
-    echo Overdose Kernel Compile Tool
+	#!/bin/bash
+	clear
+	echo Overdose Kernel Compile Tool
 	echo By MrDarkness
 	echo Please Sit Back And Chill...
 	export MAIN=`readlink -f ../`
 	export KERNELDIR=`readlink -f .`
 	rm -rf $KERNELDIR/built
 	mkdir -p $KERNELDIR/built/
-	make ARCH=arm64 X00T_defconfig
-	make -j$(nproc --all) ARCH=arm64 \
-                      CC="$MAIN/dragontc/bin/clang" \
-                      CLANG_TRIPLE=aarch64-linux-gnu- \
-                      CROSS_COMPILE="$MAIN/aarch64-linux-gnu/bin/aarch64-linux-gnu-"
-	rm -rf $KERNELDIR/built
-	mkdir -p $KERNELDIR/built/
+	export CROSS_COMPILE=$MAIN/aarch64-linux-android-4.9/bin/aarch64-linux-android-   
+	export ARCH=arm64
+	make X00T_defconfig
+	make -j$(nproc --all) | tee $KERNELDIR/built/kernel_output.txt
 	mv $KERNELDIR/arch/arm64/boot/Image.gz-dtb $KERNELDIR/built/Image.gz-dtb
-
 	echo ""
 	echo "Compile Done"
 
