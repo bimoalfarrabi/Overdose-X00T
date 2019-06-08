0b07b36f6761451919c87b50ae49f569dfcd14e0
DarknessKiller <kaelvinchen2@gmail.com>
2019-06-08 11:52:01 +0800

    Revert "cpufreq: cpu_input_boost: Tune prefer_idle for foreground cgroup"
    This reverts commit 847b156857f5a2ef85f04f9b3fca85d3f5ab469b.

--- drivers/cpufreq/cpu_input_boost.c
+++ drivers/cpufreq/cpu_input_boost.c
@@ -189,7 +189,6 @@ static void input_boost_worker(struct work_struct *work)
 		stune_boost_active = true;
 
 		do_prefer_idle("top-app", 1);
-		do_prefer_idle("foreground", 1);
 #endif
 	queue_delayed_work(b->wq, &b->input_unboost,
 			   msecs_to_jiffies(input_boost_duration));
@@ -207,7 +206,6 @@ static void input_unboost_worker(struct work_struct *work)
 		stune_boost_active = false;
 
 		do_prefer_idle("top-app", 0);
-		do_prefer_idle("foreground", 0);
 	}
 #endif
 	update_online_cpu_policy();
@@ -227,7 +225,6 @@ static void max_boost_worker(struct work_struct *work)
 		stune_boost_active = true;
 
 		do_prefer_idle("top-app", 1);
-		do_prefer_idle("foreground", 1);
 #endif
 	queue_delayed_work(b->wq, &b->max_unboost,
 		msecs_to_jiffies(atomic_read(&b->max_boost_dur)));
@@ -245,7 +242,6 @@ static void max_unboost_worker(struct work_struct *work)
 		stune_boost_active = false;
 
 		do_prefer_idle("top-app", 0);
-		do_prefer_idle("foreground", 0);
 	}
 #endif
 	update_online_cpu_policy();
@@ -356,7 +352,6 @@ static void cpu_input_boost_input_disconnect(struct input_handle *handle)
 		stune_boost_active = false;
 
 		do_prefer_idle("top-app", 0);
-		do_prefer_idle("foreground", 0);
 	}
 #endif
 	input_close_device(handle);
