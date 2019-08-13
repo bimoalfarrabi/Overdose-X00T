d7e1fc458c6da4d043c4e7f5f5f5938a855213ab
DarknessKiller <kaelvinchen2@gmail.com>
2019-08-13 10:07:37 +0800

    Overdose XVI Release

--- built/anykernel.sh
+++ built/anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Overdose XV LT - Love You 3000 Times
+kernel.string=Overdose XVI - Love You 3000 Times
 do.devicecheck=1
 do.modules=0
 do.cleanup=1
