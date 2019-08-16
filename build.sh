90d9e107c716d67e9a892b7830e66b648c6ba8c8
DarknessKiller <kaelvinchen2@gmail.com>
2019-08-16 20:46:38 +0800

    XVII Beta Ongoing...

--- build.sh
+++ build.sh
@@ -17,7 +17,7 @@
 	echo "Compile Done"
 	echo ""
 	cd $KERNELDIR/built
-	zip -r Overdose-XVI-Release-X00T-`date +%Y%m%d_%H%M`.zip * -x "kernel_output.txt" "*.zip"
+	zip -r Overdose-XVII-Beta-X00T-`date +%Y%m%d_%H%M`.zip * -x "kernel_output.txt" "*.zip"
 	cd $KERNELDIR/
 	echo "Zip Done"
 
