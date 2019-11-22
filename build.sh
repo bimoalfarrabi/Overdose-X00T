d0cbd9afcadc43e279951d18f1c93d177d26ffeb
DarknessKiller <kaelvinchen2@gmail.com>
2019-11-22 19:16:23 +0800

    AnyKernel: Update Overdose Tools

--- build.sh
+++ build.sh
@@ -7,8 +7,9 @@
 	export KERNELDIR=`readlink -f .`
 	rm $KERNELDIR/built/Image.gz-dtb
 	rm $KERNELDIR/built/kernel_output.txt
-	export CROSS_COMPILE=$MAIN/aarch64-linux-gnu/bin/aarch64-linux-gnu-   
-	export ARCH=arm64
+	export CROSS_COMPILE=$MAIN/aarch64-elf-gcc/bin/aarch64-elf-  
+	export CROSS_COMPILE_ARM32=$MAIN/arm-eabi-gcc/bin/arm-eabi- 
+	export ARCH=arm64 && export SUBARCH=arm64
 	make X00T_defconfig
 	make -j$(nproc --all) | tee $KERNELDIR/built/kernel_output.txt
 	mv $KERNELDIR/arch/arm64/boot/Image.gz-dtb $KERNELDIR/built/Image.gz-dtb
@@ -16,7 +17,6 @@
 	echo "Compile Done"
 	echo ""
 	cd $KERNELDIR/built
-	zip -r Overdose-XVIII-Release-X00T-`date +%Y%m%d_%H%M`.zip * -x "kernel_output.txt" "*.zip"
+	zip -r Overdose-XVIII-Beta-X00T-`date +%Y%m%d_%H%M`.zip * -x "kernel_output.txt" "*.zip"
 	cd $KERNELDIR/
 	echo "Zip Done"
-
