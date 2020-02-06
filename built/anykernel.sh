c6cc4e8eebaff0bc11abb028961d6296d066fcc2
DarknessKiller <kaelvinchen2@gmail.com>
2020-02-06 13:28:19 +0800

    Overdose XIX.8 Release

--- built/anykernel.sh
+++ built/anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Overdose XIX.7 - Love You 3000 Times
+kernel.string=Overdose XIX.8 - Adapt & Improve
 do.devicecheck=1
 do.modules=0
 do.cleanup=1
