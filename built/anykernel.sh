15e08a380835c863f17b8be9f34b95e5a9ed0c0a
DarknessKiller <kaelvinchen2@gmail.com>
2020-02-29 14:02:31 +0800

    AnyKernel3: Updates!

--- built/anykernel.sh
+++ built/anykernel.sh
@@ -7,6 +7,7 @@ properties() { '
 kernel.string=Overdose XIX.8 - Adapt & Improve
 do.devicecheck=1
 do.modules=0
+do.systemless=1
 do.cleanup=1
 do.cleanuponabort=0
 device.name1=ASUS_X00TD
@@ -27,16 +28,11 @@ ramdisk_compression=auto;
 # import patching functions/variables - see for reference
 . tools/ak3-core.sh;
 
-# Mount partitions as rw
-mount /system;
-mount /vendor;
-mount -o remount,rw /system;
-mount -o remount,rw /vendor;
 
 ## AnyKernel file attributes
 # set permissions/ownership for included ramdisk files
-chmod -R 750 $ramdisk/*;
-chown -R root:root $ramdisk/*;
+set_perm_recursive 0 0 755 644 $ramdisk/*;
+set_perm_recursive 0 0 750 750 $ramdisk/init* $ramdisk/sbin;
 
 
 ## AnyKernel install
@@ -71,20 +67,6 @@ remove_line init.rc "import /init.pk.rc"
 remove_line init.rc "import /init.boost.rc"
 remove_line init.rc "import /init.stardust.rc"
 insert_line init.rc "import /init.overdose.rc" after "import /init.usb.configfs.rc" "import /init.overdose.rc";
-
-# end ramdisk changes
-
-## backup post-boot script
-#if [ ! -f /vendor/bin/init.qcom.post_boot.sh.bkp ]; then
-#	cp -rpf /vendor/bin/init.qcom.post_boot.sh /vendor/bin/#init.qcom.post_boot.sh.bkp;
-#fi
-
-# replace post-boot script
-#cp -rpf $patch/init.qcom.post_boot.sh /vendor/bin/init.qcom.post_boot.sh;
-
-#chmod -R 755 /vendor/bin/init.qcom.post_boot.sh;
-#chmod -R 755 /vendor/bin/init.qcom.post_boot.sh.bkp;
-
 
 # end ramdisk changes
 
