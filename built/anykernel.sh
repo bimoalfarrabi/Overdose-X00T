05f1985d79eceb29715678fb0a80e4423f4696f2
DarknessKiller <kaelvinchen2@gmail.com>
2020-04-07 11:09:11 +0800

    Overdose XXI.1 Release

--- built/anykernel.sh
+++ built/anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Overdose XX - Adapt & Improve
+kernel.string=Overdose XXI - Adapt & Improve
 do.devicecheck=1
 do.modules=0
 do.systemless=1
