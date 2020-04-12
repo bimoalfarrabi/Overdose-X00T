006e6e0b439669e102a6cc217a2a6bff82cae728
DarknessKiller <kaelvinchen2@gmail.com>
2020-04-12 12:20:23 +0800

    Overdose XXII.1 Release * Fixed Audio/Camera Issue On OOS * Fixed Wifi Slow Issue * Fixed Miui Booting Issue

--- built/anykernel.sh
+++ built/anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=Overdose XXI - Adapt & Improve
+kernel.string=Overdose XXII - Adapt & Improve
 do.devicecheck=1
 do.modules=0
 do.systemless=1
