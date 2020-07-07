4d3c36fc566d913338c8580d325a03b6c682d4b1
Syarif Ibnu <syarifibnuzaelani57@gmail.com>
2020-07-08 06:58:01 +0700

    Drivers: Power: Force input_current_settled To 3A * We Dont Want This To Reduce ICL On A Device Where Charging Is Already So Messed Up * Maybe This Is Completely Wrong...
    Signed-off-by: pranavkapoor001 <pranavkapoor001@gmail.com>
    Signed-off-by: Syarif Ibnu <syarifibnuzaelani57@gmail.com>
    Signed-off-by: viasco <bimoalfarrabi24@gmail.com>

--- drivers/power/supply/qcom/smb-lib.c
+++ drivers/power/supply/qcom/smb-lib.c
@@ -135,8 +135,11 @@ static void asus_smblib_rerun_aicl(struct smb_charger *chg)
 			USBIN_AICL_EN_BIT, 0);
 	/* Add a delay so that AICL successfully clears */
 	msleep(50);
-	smblib_masked_write(chg, USBIN_AICL_OPTIONS_CFG_REG,                          //reg=1380   bit2=0     USBIN_AICL_EN=Enable
-			USBIN_AICL_EN_BIT, USBIN_AICL_EN_BIT);
+
+	/* reg=1380, bit2=0, USBIN_AICL_EN=enable */
+	smblib_masked_write(chg, USBIN_AICL_OPTIONS_CFG_REG,
+				USBIN_AICL_EN_BIT, 
+                USBIN_AICL_EN_BIT, 1);
 }
 extern struct wake_lock asus_chg_lock;
 void asus_smblib_stay_awake(struct smb_charger *chg)
